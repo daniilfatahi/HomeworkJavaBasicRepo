bc9de3de14e80251fee68974aeb5bb491e11029d
daniilfatahi <daniil.fatahi@gmail.com>
2022-09-12 18:55:25 +0200

    Homework 3 CO

---
+++ src/com/company/Coordinates.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Coordinates {
+
+    public static void main(String[] args) {
+        double lat, lon;
+        lat = 50.46994185;
+        lon = 30.520753505168365;
+
+
+        System.out.println("My coordinates are: " + lat + "," + lon);
+
+    }
+}
