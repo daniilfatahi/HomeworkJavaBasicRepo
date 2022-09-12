05b72fb37a1cada2c6da587210ed5eafeb4120f0
daniilfatahi <daniil.fatahi@gmail.com>
2022-09-12 19:31:37 +0200

    Homework4

---
+++ src/com/company/Homework4.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Homework4 {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 12;
+        int c = 17;
+
+        int volume = a * b * c;
+        int length = (a + b + c) * 4;
+
+        System.out.println("Обʼєм паралелепіпеда = " + volume);
+        System.out.println("Сума довжин всіх сторін паралелепіпеда = " + length);
+
+
+    }
+}
+
+
+
+
