e21cb74788a76f48d4b98aa9e7dd38c145c0c166
daniilfatahi <daniil.fatahi@gmail.com>
2022-09-15 21:40:09 +0200

    Homework8

---
+++ src/com/company/Homework8.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Homework8 {
+
+    public static void main(String[] args) {
+        int min = 1;
+        int max = 6;
+        Scanner intScanner = new Scanner(System.in);
+        Random myRandom = new Random();
+        int borderRandom = myRandom.nextInt(max-min)+max;
+
+
+        System.out.println("Введіть число від 1 до 6: ");
+        int number = intScanner.nextInt();
+
+        if (borderRandom == number) {
+            System.out.println("You have won!");
+        } else if (borderRandom != number) {
+            System.out.println("You haven't guessed, needed number is:  " + borderRandom);
+        }
+
+    }
+}
