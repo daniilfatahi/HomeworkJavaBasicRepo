e4c53a0c22f6a746d47e934705c9ac7454d771b7
daniilfatahi <daniil.fatahi@gmail.com>
2022-09-24 23:55:54 +0300

    Homework9

---
+++ src/com/company/Homework9.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Homework9 {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        Scanner scanner = new Scanner(System.in);
+        boolean correct = false;
+        while (!correct) {
+
+            System.out.println("Enter number from 0 to 10");
+
+            int guess = scanner.nextInt();
+            int randomAnswer = r.nextInt(10);
+            if (guess != randomAnswer) {
+
+                System.out.println("Try again");
+
+            } else {
+                System.out.println("You have won!");
+                correct = true;
+            }
+
+        }
+        System.exit(0);
+
+    }
+}
